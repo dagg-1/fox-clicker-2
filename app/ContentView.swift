fb667f2a44fc0af8b5e72f77c1d7a7d7c729e8eb
Dagg <daggintosh@outlook.com>
2019-09-06 01:02:33 -0700

    Working clicker

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -10,52 +10,83 @@ import SwiftUI
 
 var points : Int64 = 0
 
-struct BuyableList: View {
-    var FoxVar = ""
-    var FoxVarPhrase = ""
-    @State var FoxVarNumber : Int64 = 0
-    @State var FoxVarInc : Double
-    @State var FoxVarCost : Double
-    
-    var body: some View {
-        HStack {
-            VStack {
-                Text("\(FoxVar)").font(.headline)
-                HStack {
-                    Text("Quantity: \(FoxVarNumber)").font(.subheadline)
-                    Divider()
-                    Text("Cost: \(Int64(FoxVarCost))").font(.subheadline)
-                    Divider()
-                    Button(action: {
-                        if(points >= Int64(self.FoxVarCost)) {
-                            self.FoxVarNumber += 1
-                            points -= Int64(self.FoxVarCost)
-                            self.FoxVarCost *= self.FoxVarInc
+class thatclass {
+    struct BuyableList: View {
+        var FoxVar = ""
+        var FoxVarPhrase = ""
+        @State var FoxVarNumber : Int64 = 0
+        @State var FoxVarInc : Double
+        @State var FoxVarCost : Double
+        @State var FoxVarTimer : Int
+        
+        var body: some View {
+            HStack {
+                VStack {
+                    Text("\(FoxVar)").font(.headline)
+                    HStack {
+                        Text("Quantity: \(FoxVarNumber)").font(.subheadline)
+                        Divider()
+                        Text("Cost: \(Int64(FoxVarCost))").font(.subheadline)
+                        Divider()
+                        Button(action: {
+                            if(points >= Int64(self.FoxVarCost)) {
+                                self.FoxVarNumber += 1
+                                points -= Int64(self.FoxVarCost)
+                                self.FoxVarCost *= self.FoxVarInc
+                                Timer.scheduledTimer(withTimeInterval: Double(self.FoxVarTimer), repeats: true) {
+                                    [self] _ in
+                                    
+                                    switch (self.FoxVar) {
+                                    case "Fox Farm":
+                                        points += 1
+                                        break
+                                    case "Fox Factory":
+                                        points += 5
+                                        break
+                                    default:
+                                        break
+                                    }
+                                }
+                            }
+                        }) {
+                            Text("\(FoxVarPhrase)")
                         }
-                    }) {
-                        Text("\(FoxVarPhrase)")
                     }
+                    Divider()
                 }
             }
         }
     }
-}
-
-struct ContentView: View {
-    @State var content: Int64 = points
-    var body: some View {
-        HStack {
-            List() {
-                BuyableList(FoxVar: "Fox Farm", FoxVarPhrase: "Plant", FoxVarInc: 1.02, FoxVarCost: Double(100))
-                BuyableList(FoxVar: "Fox Factory", FoxVarPhrase: "Build", FoxVarInc: 1.08, FoxVarCost: Double(200))
+    
+    struct ContentView: View {
+        @State var content: Int64 = points
+        
+        func updater() {
+            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) {
+                [self] _ in
+                self.content = points
             }
-            Divider()
-            VStack {
-                Button(action: { points += 1; print(points) }) {
-                    Text("Spawn Fox")
+        }
+
+        var body: some View {
+            HStack {
+                List() {
+                    thatclass.BuyableList(FoxVar: "Fox Farm", FoxVarPhrase: "Plant", FoxVarInc: 1.02, FoxVarCost: Double(100), FoxVarTimer: 3)
+                    thatclass.BuyableList(FoxVar: "Fox Factory", FoxVarPhrase: "Build", FoxVarInc: 1.08, FoxVarCost: Double(200), FoxVarTimer: 1)
                 }
-                Text("\(content) Points")
-            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                Divider()
+                VStack {
+                    Button(action: {
+                        points += 1
+                        if (points == 1) {
+                            self.updater()
+                        }
+                    }) {
+                        Text("Spawn Fox")
+                    }
+                    Text("\(content) Points")
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
         }
     }
 }
@@ -63,6 +94,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        thatclass.ContentView()
     }
 }
