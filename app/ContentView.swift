aef1462e4289604ac630c2225359b09fa6a3c3f9
Dagg <daggintosh@outlook.com>
2019-09-06 12:16:27 -0700

    Tweaks

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -34,37 +34,48 @@ class thatclass {
                                 self.FoxVarNumber += 1
                                 points -= Int64(self.FoxVarCost)
                                 self.FoxVarCost *= self.FoxVarInc
-                                Timer.scheduledTimer(withTimeInterval: Double(self.FoxVarTimer), repeats: true) {
-                                    [self] _ in
-                                    
-                                    switch (self.FoxVar) {
-                                    case "Fox Farm":
-                                        points += 1
-                                        break
-                                    case "Fox Factory":
-                                        points += 5
-                                        break
-                                    case "Fox Printer":
-                                        points += 10
-                                        break
-                                    case "Fox Matter Fabricator":
-                                        points += 20
-                                        break
-                                    case "Fox Mass Cloner":
-                                        points += 50
-                                        break
-                                    case "International Fox Operations":
-                                        points += 150
-                                        break
-                                    case "Lunar Fox Mines":
-                                        points += 500
-                                        break
-                                    case "Fox Planet Shipments":
-                                        points += 2000
-                                        break
-                                    default:
-                                        // Having a default case is explictly required.
-                                        break
+                                if (self.FoxVarNumber == 1) {
+                                    Timer.scheduledTimer(withTimeInterval: Double(self.FoxVarTimer), repeats: true) {
+                                        [self] _ in
+                                        
+                                        var incam : Int64
+                                        switch (self.FoxVar) {
+                                        case "Fox Farm":
+                                            incam = self.FoxVarNumber * 1
+                                            points += incam
+                                            break
+                                        case "Fox Factory":
+                                            incam = self.FoxVarNumber * 5
+                                            points += incam
+                                            break
+                                        case "Fox Printer":
+                                            incam = self.FoxVarNumber * 10
+                                            points += incam
+                                            break
+                                        case "Fox Matter Fabricator":
+                                            incam = self.FoxVarNumber * 20
+                                            points += incam
+                                            break
+                                        case "Fox Mass Cloner":
+                                            incam = self.FoxVarNumber * 50
+                                            points += incam
+                                            break
+                                        case "International Fox Operations":
+                                            incam = self.FoxVarNumber * 150
+                                            points += incam
+                                            break
+                                        case "Lunar Fox Mines":
+                                            incam = self.FoxVarNumber * 500
+                                            points += incam
+                                            break
+                                        case "Fox Planet Shipments":
+                                            incam = self.FoxVarNumber * 2000
+                                            points += incam
+                                            break
+                                        default:
+                                            // Having a default case is explictly required.
+                                            break
+                                        }
                                     }
                                 }
                             }
@@ -96,9 +107,9 @@ class thatclass {
                     thatclass.BuyableList(FoxVar: "Fox Printer", FoxVarPhrase: "Buy", FoxVarInc: 1.2, FoxVarCost: 300.0, FoxVarTimer: 2)
                     thatclass.BuyableList(FoxVar: "Fox Matter Fabricator", FoxVarPhrase: "Create", FoxVarInc: 2.0, FoxVarCost: 500.0, FoxVarTimer: 1)
                     thatclass.BuyableList(FoxVar: "Fox Mass Cloner", FoxVarPhrase: "Build", FoxVarInc: 2.0, FoxVarCost: 2000.0, FoxVarTimer: 1)
-                    thatclass.BuyableList(FoxVar: "International Fox Operations", FoxVarPhrase: "Negotiate", FoxVarInc: 5.0, FoxVarCost: 5000.00, FoxVarTimer: 10)
-                    thatclass.BuyableList(FoxVar: "Lunar Fox Mines", FoxVarPhrase: "Build", FoxVarInc: 8.0, FoxVarCost: 50000.0, FoxVarTimer: 15)
-                    thatclass.BuyableList(FoxVar: "Fox Planet Shipments", FoxVarPhrase: "Ship", FoxVarInc: 10.0, FoxVarCost: 100000.0, FoxVarTimer: 20)
+                    thatclass.BuyableList(FoxVar: "International Fox Operations", FoxVarPhrase: "Negotiate", FoxVarInc: 5.0, FoxVarCost: 5000.00, FoxVarTimer: 3)
+                    thatclass.BuyableList(FoxVar: "Lunar Fox Mines", FoxVarPhrase: "Build", FoxVarInc: 8.0, FoxVarCost: 50000.0, FoxVarTimer: 3)
+                    thatclass.BuyableList(FoxVar: "Fox Planet Shipments", FoxVarPhrase: "Ship", FoxVarInc: 10.0, FoxVarCost: 100000.0, FoxVarTimer: 3)
                 }
                 Divider().padding(.top).padding(.bottom)
                 VStack {
